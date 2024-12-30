54bcc09d396b4519c1a9ec07514f4db3de5972b7
cheetosfan4 <marcmclennan413@gmail.com>
2024-12-30 00:33:12 -0500

    room3
    room3

--- Assets/cameramove.cs
+++ Assets/cameramove.cs
@@ -6,7 +6,10 @@ public class CameraController : MonoBehaviour
 {
     public Transform player; // Reference to the player transform
     public Vector3 defaultCameraPosition = new Vector3(0, 8, 13); // Default camera position
+	public Vector3 defaultCameraRotation = new Vector3(25, 180, 0);
     public Vector3 alternateCameraPosition = new Vector3(-18, 8, 13); // Alternate camera position
+	public Vector3 alternateCameraRotation = new Vector3(25, 215, 0);
+	public Vector3 alternateCameraPosition2 = new Vector3(25, 8, 7);
 
     void Update()
     {
@@ -16,11 +19,17 @@ public class CameraController : MonoBehaviour
             if (player.position.x < -8.5)
             {
                 transform.position = alternateCameraPosition;
+				transform.rotation = Quaternion.Euler(defaultCameraRotation);
             }
-            else
+            else if (player.position.x > -8.5 && player.position.x < 8.5)
             {
                 transform.position = defaultCameraPosition;
+				transform.rotation = Quaternion.Euler(defaultCameraRotation);
             }
+			else if (player.position.x > 8.5) {
+				transform.position = alternateCameraPosition2;
+				transform.rotation = Quaternion.Euler(alternateCameraRotation);
+			}
         }
     }
 }
